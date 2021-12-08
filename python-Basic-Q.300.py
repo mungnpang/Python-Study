724ee4b51cc5eb2fd9dd0ac6990e4c36776d07a7
mungnpang <92630511+mungnpang@users.noreply.github.com>
2021-12-08 23:20:52 +0900

    Add files via upload

--- python-Basic-Q.300.py
+++ python-Basic-Q.300.py
@@ -760,7 +760,7 @@
 
 
 # no.100 date와 close_price 두 개의 리스트를 close_table 이름의 딕셔너리로 생성하라.
-date = ['09/05', '09/06', '09/07', '09/08', '09/09']
-close_price = [10500, 10300, 10100, 10800, 11000]
-close_table = dict(zip(date, close_price))
-print(close_table)
+# date = ['09/05', '09/06', '09/07', '09/08', '09/09']
+# close_price = [10500, 10300, 10100, 10800, 11000]
+# close_table = dict(zip(date, close_price))
+# print(close_table)
