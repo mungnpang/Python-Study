330438589af0f54b233b8a514cf6fd046d69105c
mungnpang <92630511+mungnpang@users.noreply.github.com>
2021-12-13 19:39:51 +0900

    Add files via upload

--- python-Basic-Q.300.py
+++ python-Basic-Q.300.py
@@ -1411,3 +1411,104 @@
 #     print(i)
 
 
+# no.151 리스트에는 네 개의 정수가 저장돼 있다.
+# 리스트 = [3, -20, -3, 44]
+
+# for문을 사용해서 리스트의 음수를 출력하라.
+# -20
+# -3
+
+# for i in 리스트:
+#     if i < 0:
+#         print(i)
+
+
+# no.152 for문을 사용해서 3의 배수만을 출력하라.
+# 리스트 = [3, 100, 23, 44]
+#>>> 3
+
+# for i in 리스트:
+#     if i%3 == 0:
+#         print(i)
+
+
+# no.153 리스트에서 20 보다 작은 3의 배수를 출력하라
+# 리스트 = [13, 21, 12, 14, 30, 18]
+# 12
+# 18
+
+# for i in 리스트:
+#     if i < 20 and i%3 == 0:
+#         print(i)
+
+
+# no.154 리스트에서 세 글자 이상의 문자를 화면에 출력하라
+# 리스트 = ["I", "study", "python", "language", "!"]
+# study
+# python
+# language
+
+# for i in 리스트:
+#     if len(i) >= 3:
+#         print(i)
+
+
+# no.155 리스트에서 대문자만 화면에 출력하라.
+# 리스트 = ["A", "b", "c", "D"]
+# A
+# D
+
+# for i in 리스트:
+#     if i.isupper():
+#         print(i)
+
+
+# no.156 리스트에서 소문자만 화면에 출력하라.
+# 리스트 = ["A", "b", "c", "D"]
+# b
+# c
+
+# for i in 리스트:
+#     if i.islower():
+#         print(i)
+
+
+# no.157 이름의 첫 글자를 대문자로 변경해서 출력하라.
+# 리스트 = ['dog', 'cat', 'parrot']
+# Dog
+# Cat
+# Parrot
+
+# for i in 리스트:
+#     print(i.capitalize())
+
+
+# no.158 파일 이름이 저장된 리스트에서 확장자를 제거하고 파일 이름만 화면에 출력하라. (힌트: split() 메서드)
+# 리스트 = ['hello.py', 'ex01.py', 'intro.hwp']
+# hello
+# ex01
+# intro
+
+# for i in 리스트:
+#     print(i.split('.')[0])
+
+
+# no.159 파일 이름이 저장된 리스트에서 확장자가 .h인 파일 이름을 출력하라.
+# 리스트 = ['intra.h', 'intra.c', 'define.h', 'run.py']
+# intra.h
+# define.h
+
+# for i in 리스트:
+#     if i.split('.')[1] == 'h':
+#         print(i)
+
+
+# no.160 파일 이름이 저장된 리스트에서 확장자가 .h나 .c인 파일을 화면에 출력하라.
+# 리스트 = ['intra.h', 'intra.c', 'define.h', 'run.py']
+# intra.h
+# intra.c
+# define.h
+
+# for i in 리스트:
+#     if i.split('.')[1] == 'h' or i.split('.')[1] == 'c':
+#         print(i)
